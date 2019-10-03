02f7c723e122580b3b36340d30608ffe068d7998
jazza <jazza0304@gmail.com>
2019-10-03 11:08:04 -0500

    service ,repository final, con raul

--- equipo2semana4-pedidos/src/main/java/com/everis/equipo2semana4/pedidos/Repository/ProductoRepository.java
+++ equipo2semana4-pedidos/src/main/java/com/everis/equipo2semana4/pedidos/Repository/ProductoRepository.java
@@ -3,9 +3,10 @@ package com.everis.equipo2semana4.pedidos.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.everis.equipo2semana4.pedidos.model.Pedido;
+import com.everis.equipo2semana4.pedidos.model.Producto;
+
 
 @Repository
-public interface ProductoRepository extends JpaRepository <Pedido,Integer>{
+public interface ProductoRepository extends JpaRepository <Producto,Integer>{
 
 }
