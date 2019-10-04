529b1309985d217b97ed379e6e97f56865fa850a
jazza <jazza0304@gmail.com>
2019-10-04 09:17:02 -0500

    swagger

--- equipo2semana4-inventario/src/main/java/com/everis/equipo2semana4/inventario/Equipo2semana4InventarioApplication.java
+++ equipo2semana4-inventario/src/main/java/com/everis/equipo2semana4/inventario/Equipo2semana4InventarioApplication.java
@@ -3,13 +3,22 @@ package com.everis.equipo2semana4.inventario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Import(SwaggerConfiguration.class)
-public class Equipo2semana4InventarioApplication {
 
+public class Equipo2semana4InventarioApplication implements WebMvcConfigurer{
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Equipo2semana4InventarioApplication.class, args);
 	}
 
+	  @Override
+	  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	         registry.addResourceHandler("swagger-ui.html")
+	                  .addResourceLocations("classpath:/META-INF/resources/");
+	  }
 }
